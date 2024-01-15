d6d6d469b162d7bb376b99b2baa38501db9c3860
Kishan Parmar <kparmar2101@gmail.com>
2024-01-15 22:35:29 +0530

    rename sorting to algorithms

--- algorithms/merge_sort.cpp
+++ algorithms/merge_sort.cpp
@@ -11,19 +11,22 @@ void printArr(int * arr, int size) {
   std::cout << "}" << std::endl;
 }
 
+void merge(int arr[], int left, int mid, int right) {
 
-void selection_sort(int * arr, int size) {
+  int *merge_arr = new int[right-left+1];
 
-  for (int idx = 1; idx < size; idx++) {
+  int idx = 0;
+  ZZ
+}
 
-    int minIdx = idx - 1;
-    for (int jdx = minIdx+1 ; jdx<size; jdx++)
-      if(arr[jdx]< arr[minIdx])
-        minIdx  = jdx;
-    int temp = arr[minIdx];
-    arr[minIdx] = arr[idx-1];
-    arr[idx-1]    = temp;
-  }
+void merge_sort(int arr[], int left, int right) {
+
+  if (left<=right) return;
+
+  int mid = (right-left)/2;
+  merge_sort(arr, left, mid);
+  merge_sort(arr, mid+1,right);
+  merge(arr, left, mid, right);
 }
 
 int main() {
@@ -32,11 +35,13 @@ int main() {
 
   std::size_t arr_size = sizeof(arr)/sizeof(arr[0]);
 
+  int left = 0, right = arr_size -1;
+
   std::cout << "Array Before sorting : ";
   printArr(arr, arr_size);
 
-  selection_sort(arr, arr_size);
-  
+  merge_sort(arr, left, right); 
+
   std::cout << "Array After sorting  : ";
   printArr(arr, arr_size);
 }
